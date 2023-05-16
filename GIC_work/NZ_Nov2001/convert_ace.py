aea03020b6a40f689112b68906bf051fd8d7f810
Dan Welling <dwelling@umich.edu>
2023-05-16 17:02:50 -0400

    File reader created successfully.

--- GIC_work/NZ_Nov2001/convert_ace.py
+++ GIC_work/NZ_Nov2001/convert_ace.py
@@ -5,11 +5,18 @@ This code combines the SWEPAM STI data with the high-resolution magnetometer
 data.
 '''
 
+from datetime import datetime, timedelta
+import numpy as np
+from numpy.ma import masked_less
 
-def read_sti():
+
+def load_ace_sti():
     '''
-    Read the ACE STI data.
+    Read the ACE STI data file and parse into
     '''
+
+    badval = -9999
+    varnames = ['n', 't', 'v', 'vx', 'vy', 'vz', 'b', 'bx', 'by', 'bz']
 
     with open('./ACE_raw_2001308.dat', 'r') as f:
         # Skip header:
@@ -18,5 +25,28 @@ def read_sti():
             line = f.readline()
 
         # Slurp rest of lines:
-        lines = line + f.readlines()
+        lines = [line] + f.readlines()
 
+    # Create data container.
+    nlines = len(lines)
+    data = {}
+    data['time'] = np.zeros(nlines, dtype=object)
+    for v in varnames:
+        data[v] = np.zeros(nlines)
+
+    # Loop over lines, parse data.
+    for i, l in enumerate(lines):
+        parts = l.split()
+
+        # Time entry:
+        data['time'][i] = datetime(int(parts[0]), 1, 1, 0, 0, 0)
+        data['time'][i] += timedelta(days=float(parts[1]) + float(parts[2]))
+
+        for j, v in enumerate(varnames):
+            data[v][i] = parts[j+3]
+
+    # Mask bad data:
+    for v in varnames:
+        data[v] = masked_less(data[v], badval)
+
+    return data
