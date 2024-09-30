9428720d077523f1860db34b02fac2be43c83722
Dan Welling <dwelling@umich.edu>
2024-09-30 09:21:06 -0400

    A version that compiles. Nothing more.

--- pointsource/ModUserPointSource.f90
+++ pointsource/ModUserPointSource.f90
@@ -7,18 +7,17 @@ module ModUser
   ! Should be 1 "implemented" for each subroutine defined, in order that
   ! changes BATS defaults (e.g., not "user_actions", etc.)
   use ModUserEmpty,               &
-       IMPLEMENTED1  => user_read_inputs,                &
-       IMPLEMENTED2  => user_init_session,               &
-       IMPLEMENTED3  => user_calc_sources_expl,          &
-       IMPLEMENTED4  => user_calc_sources_impl
+       IMPLEMENTED1  => user_action,                     &
+       IMPLEMENTED2  => user_read_inputs,                &
+       IMPLEMENTED3  => user_init_session,               &
+       IMPLEMENTED4  => user_calc_sources_expl,          &
+       IMPLEMENTED5  => user_calc_sources_impl
 
   use BATL_lib, ONLY: &
        test_start, test_stop, &
        iTest, jTest, kTest, iBlockTest, iProcTest, iVarTest, iProc
-  use ModSize
-  use ModNumConst,  ONLY: cPi, cTiny
-  use ModAdvance,   ONLY: Pe_, UseElectronPressure
-  use ModMultiFluid
+  ! use ModSize
+  use ModNumConst,  ONLY: cPi
 
   include 'user_module.h' ! list of public methods
 
@@ -29,152 +28,11 @@ module ModUser
        'Point mass source; DTW 2024'
 
   ! DTW Variable defs
-  real :: XyzSource(3) = 0  ! Location of point sources.
-  ! ---------
-
-  ! Use the CG shape or not. If not, then use a spherical body.
-  logical :: DoUseCGShape = .true.
-  real    :: rSphericalBodySi = 2.0e3
-  real    :: rSphericalBody
-
-  integer:: nTriangle
-  real, allocatable:: XyzTriangle_DII(:,:,:), Normal_DI(:,:)
-  real :: rMinShape = 0.0, rMaxShape = 0.0
-
-  ! Position of the sun at the start time
-  real :: LatSun=43.5, LonSun=0.0
-  real :: NormalSun_D(3)
-
-  ! Rotation matrix to rotate the comet so that the Sun is in the +x direction.
-  real:: Rot_DD(3,3) = 0.
-
-  ! Rotation of the comet (changes the direction of the Sun)
-  real:: RotationCometHour = 12.0
-
-  ! Angular velocity
-  real:: OmegaComet
-
-  ! Maximum change in longitude before updating the boundary conditions
-  real:: AngleUpdateDeg = 10.0
-
-  ! The time between updates
-  real:: DtUpdateSi
-
-  ! minimum and maximum temperature
-  real :: TempCometMinDim, TempCometMaxDim, TempCometMin, TempCometMax
-
-  ! Temperature at 75.5 degree to determin the temperature slope
-  real :: TempComet75Dim, TempComet75
-
-  ! Minimum and maximum production rate
-  real :: ProductionRateMaxSi, ProductionRateMinSi
-  real :: ProductionRateMax, ProductionRateMin
-
-  ! Maximum solar zenith angle for dayside production rate
-  real :: SolarAngleMaxDim, SolarAngleMax
-
-  ! Parameters for y=ax+b to mimic the production rate and
-  ! temperature distribution
-  real :: SlopeProduction, bProduction, SlopeTemp, bTemp
-
-  ! Constant parameters to calculate uNormal and temperature
-  ! from TempCometLocal
-  real :: TempToUnormal
-  real :: TempToPressure
-
-  ! Inner boundary condition for ions
-  character (len=10) :: TypeBodyBC = 'default'
-  logical :: UseSwBC        = .false.
-  logical :: UseReflectedBC = .false.
-
-  ! Inner boundary condition for magnetic field
-  character (len=10) :: TypeBfieldBody = 'zero'
-
-  ! Perturbed initial condition parameters:
-  real :: R0PerturbedSi  = 1e6, R0Perturbed
-  real :: R1PerturbedSi  = 2e6, R1Perturbed
-  real :: ratioPerturbed = 1e-10
-
-  ! Parameters to increase the neutral density in the source term to
-  ! approach the steady state solution more easily (hopefully)
-  logical :: DoEnhanceNeu    = .false.
-  real    :: EnhancedRatio   = 20
-  integer :: DecadeDn        = 10000
-  integer :: nStepEnhanceNeu = -100
-
-  ! minimum temperature for neutral
-  real :: TempNeuMinSi = 60.0, TempNeuMin
-
-  ! FaceCoordsTest_D
-  real :: FaceCoordsX=0.0, FaceCoordsY=0.0, FaceCoordsZ=0.0
-  real :: FaceCoordsTest_D(3) = [0.0, 0.0, 0.0]
-
-  ! Last step and time the inner boundary values were saved for each block
-  integer, allocatable :: nStepSave_B(:)
-
-  real, allocatable :: TimeSimulationSave_B(:)
-
-  integer, allocatable :: nStepSaveCalcRates_B(:)
-  integer :: nStepPritSetFace = -100
-
-  ! If this variable is set .true., then use the Haser neutral background
-  ! Only for testing purpose
-  logical :: DoUseHaserBackground = .false.
-
-  ! If this variable is set .true., then use the uniform neutral background
-  ! Only for testing purpose
-  logical :: DoUseUniformNeuBackground = .false.
-  real :: nNeuUniformSi, UxNeuUniformSi, UyNeuUniformSi, UzNeuUniformSi, &
-       TNeuUniformSi
-  real :: nNeuUniform, UxNeuUniform, UyNeuUniform, UzNeuUniform, &
-       pNeuUniform
-
-  ! Increase ionization near a field line
-  logical :: DoUseFieldlineFile = .false.
-  character (len=100) :: NameFieldlineFile
-  integer :: nVarFieldlineFile
-  real    :: RadiusTubeSI, RadiusTube, SPeAdditionalSi=5.0e-9, SPeAdditional
-  real, allocatable::  XyzFieldline_DI(:,:)
-  real    :: TimeEnhanceStartSI = 180.0, TimeEnhanceEndSI = 240.0
-  real    :: TimeEnhanceStart, TimeEnhanceEnd
-
-  integer, parameter, public :: nNeuFluid = 1
-  integer, parameter :: Neu1_  =  1
-
-  !! Ion species names
-  integer, parameter :: SW_   =  1
-  integer, parameter :: H2Op_ =  2
-
-  real :: Qprod = 1e22
-  real :: TminSi, Tmin, rHelio, vHI, uHaser
-
-  real, allocatable :: ne20eV_GB(:,:,:,:)
-
-  !! logical variable to determine whether to use an artifical perturbed solar
-  !! wind at beyond a certain xPerturbedSwIO
-  logical :: UsePerturbedSW = .false.
-  real :: xPerturbedSwMinIO = 1.5e4, xPerturbedSwMaxIO = 1.6e4
-  real :: PerturbedSwNIO = 10.0
-  real :: PerturbedSwUxIO = 400.0, PerturbedSwUyIO = 0.0, PerturbedSwUzIO = 0.0
-  real :: PerturbedSwTIO = 1.5e5, PerturbedSwTeIO = 1.5e5
-  real :: PerturbedSwBxIO = 5.0, PerturbedSwByIO = 0.0, PerturbedSwBzIO = 0.0
-  real :: xPerturbedSwMin, xPerturbedSwMax, PerturbedSwN, PerturbedSwRho
-  real :: PerturbedSwT,  PerturbedSwTe
-  real :: PerturbedSwUx, PerturbedSwUy, PerturbedSwUz
-  real :: PerturbedSwBx, PerturbedSwBy, PerturbedSwBz
-
-  !! Make the photo- and electron impact ionization rate global arrays for
-  !! user_set_plot_var
-  real, allocatable :: v_IIGB(:,:,:,:,:,:), ve_IIGB(:,:,:,:,:,:)
-
-  logical            :: DoSaveSource = .false.
-  character (len=10) :: NameSource   = 'none'
-  real, allocatable :: TestArray_IGB(:,:,:,:,:)
-
-  real, allocatable :: SPeAdditional_GB(:,:,:,:)
-
-  character (len=6), parameter, public :: NameNeutral_I(nNeuFluid) = &
-       [ 'Neu1  ' ]
+  logical, save :: UsePointSource
+  integer, save :: nPointSource = 0  ! Number of point sources.
+  ! Location and amplitude of sources:
+  real, allocatable, save :: Amplitude_I(:), XyzSource_DI(:,:)
+  real :: rspread = 1.0  ! Spread of gaussian in RE.
 
 contains
   !============================================================================
@@ -204,6 +62,9 @@ contains
 
     character (len=100) :: NameCommand
 
+    real :: sourceAmplitude
+    integer :: i
+
     logical:: DoTest
     character(len=*), parameter:: NameSub = 'user_read_inputs'
     !--------------------------------------------------------------------------
@@ -213,8 +74,25 @@ contains
        if(.not.read_line() ) EXIT
        if(.not.read_command(NameCommand)) CYCLE
        select case(NameCommand)
-       case("#SHAPEFILE")
-          call read_var('NameShapeFile' ,NameShapeFile)
+       case("#POINTMASSSOURCE")
+          call read_var('UsePointSource', UsePointSource)
+          if(.not.UsePointSource)continue
+          call read_var('nPointSource', nPointSource)
+          ! Initialize arrays:
+          allocate(XyzSource_DI(nPointSource, 3))
+          allocate(Amplitude_I(nPointSource))
+          XyzSource_DI = 0
+          Amplitude_I = 0
+          if(DoTest) write(*,*) "READING POINT SOURCE INFO"
+          do i=1, nPointSource
+              call read_var('SourceAmplitude', Amplitude_I(i))
+              call read_var('xPosition', XyzSource_DI(i, 1))
+              call read_var('yPosition', XyzSource_DI(i, 2))
+              call read_var('zPosition', XyzSource_DI(i, 3))
+              if(DoTest) write(*,'(a,i02,a,3f10.7)') 'Source #', i, &
+                   ' located at xyz=', XyzSource_DI(i, :)
+          end do
+
        case('#USERINPUTEND')
           EXIT
        case default
@@ -230,30 +108,10 @@ contains
 
     ! Read shape file and convert units
 
-    use ModMain, ONLY: tSimulation, nStep
-    use ModPhysics, ONLY: Io2No_V, Si2No_V, No2Si_V, &
-         UnitU_, UnitTemperature_, UnitT_, UnitP_,   &
-         UnitN_, UnitX_, UnitB_, UnitEnergyDens_
-    use ModNumConst, ONLY: cTwoPi, cDegToRad
-    use ModCoordTransform, ONLY: dir_to_xyz
-    use ModConst, ONLY: cBoltzmann, cAtomicMass
-    use ModVarIndexes, ONLY: MassFluid_I
-    use ModBlockData, ONLY: MaxBlockData
-    use ModIO, ONLY: IsRestart
-    use ModCoordTransform, ONLY: rot_matrix_y, rot_matrix_z
-
     logical:: DoTest
     character(len=*), parameter:: NameSub = 'user_init_session'
     !--------------------------------------------------------------------------
     call test_start(NameSub, DoTest)
-
-    ! Obtained the rotation matrix from LatSun and LonSun
-    Rot_DD = matmul( rot_matrix_z(-LonSun*cDegToRad), &
-         rot_matrix_y(LatSun*cDegToRad) )
-
-    ! Now the Sun is in the +x direction
-    LatSun = 0
-    LonSun = 0
 
     ! Test statement, need to remove later
     if (iProc == 0) then
@@ -267,16 +125,12 @@ contains
   subroutine user_calc_sources_impl(iBlock)
 
     use ModMain,       ONLY: nI, nJ, nK, tSimulation
-    use ModAdvance,    ONLY: State_VGB, Source_VC, Bx_, By_, Bz_, P_
-    use ModConst,      ONLY: cBoltzmann, cElectronMass, cProtonMass, cEV
+    use ModAdvance,    ONLY: State_VGB, Source_VC, p_, Rho_
     use ModGeometry,   ONLY: Xyz_DGB
-    use ModCurrent,    ONLY: get_current
     use ModPhysics,    ONLY: Si2No_V, No2Si_V, &
          UnitEnergyDens_, UnitN_, UnitRho_, UnitU_, UnitP_, UnitT_, &
          UnitRhoU_, UnitTemperature_, UnitX_,       &
          ElectronPressureRatio, ElectronCharge
-    use ModVarIndexes,    ONLY: MassFluid_I
-    use ModBlockData,     ONLY: use_block_data, get_block_data, put_block_data
 
     integer, intent(in) :: iBlock
 
@@ -285,84 +139,44 @@ contains
 
     ! Local source arrays
     real, dimension(1:nI,1:nJ,1:nK) :: SRho_C, SP_C
-    !------
 
-    integer, parameter :: nRhoTerm=5, nRhoUTerm=7, nPTerm=11, nPeTerm =10
-
-    real, dimension(1:nI,1:nJ,1:nK) :: nElec_C, Te_C, SBx_C, SBy_C, SBz_C, &
-         SPe_C, TempNeu1_C, nNeu1_C
-
-    real, dimension(nRhoTerm, 1:nIonFluid,1:nI,1:nJ,1:nK) :: SRhoTerm_IIC
-    real, dimension(nRhoUTerm,1:nIonFluid,1:nI,1:nJ,1:nK) :: SRhoUxTerm_IIC, &
-         SRhoUyTerm_IIC, SRhoUzTerm_IIC
-    real, dimension(nPTerm,1:nIonFluid,1:nI,1:nJ,1:nK) :: SPTerm_IIC
-    real, dimension(nPeTerm,           1:nI,1:nJ,1:nK) :: SPeTerm_IC
-
-    real, dimension(1:3,1:nIonFluid,1:nI,1:nJ,1:nK) :: uIon_DIC
-
-    real, dimension(1:3,1:nI,1:nJ,1:nK) :: Current_DC, uIonMean_DC, uElec_DC, &
-         uNeu1_DC
-
-    real, dimension(1:nIonFluid,1:nIonFluid,1:nI,1:nJ,1:nK) :: &
-         fii_IIC, uIonIon2_IIC
-
-    real, dimension(1:nIonFluid,            1:nI,1:nJ,1:nK) :: alpha_IC
-    real, dimension(1:nIonFluid,1:nNeuFluid, 1:nI,1:nJ,1:nK) :: &
-         fin_IIC, uIonNeu2_IIC
-    real, dimension(            1:nNeuFluid, 1:nI,1:nJ,1:nK) :: &
-         fen_IC, uNeuElec2_IC
-
-    real, dimension(1:nNeuFluid,1:nIonFluid) :: Qexc_II, Qion_II
-
-    real, dimension(1:nIonFluid) :: fiiTot_I, finTot_I, vAdd_I, veAdd_I, &
-         kinAdd_I, kinSub_I
-    !--------------------------------------------------------------------------
-    real, dimension &
-         (1:nIonFluid,1:nNeuFluid,1:nNeuFluid,1:nIonFluid,1:nI,1:nJ,1:nK) :: &
-         kin_IIIIC
-
-    real    :: fenTot, feiTot
-    integer :: i,j,k,iNeuFluid,jNeutral,iIonFluid,jIonFluid,iTerm
+    real :: distNow_D(3)
 
     logical :: DoTestCell
-    logical :: DoCalcShading = .false.
-    integer, save :: iBlockLast = -100
-    integer, save :: iLastDecomposition = -100
-    real,    save :: IsIntersectedShapeR_III(nI,nJ,nK) = -1.0
 
     logical:: DoTest
     character(len=*), parameter:: NameSub = 'user_calc_sources_impl'
     !--------------------------------------------------------------------------
     call test_start(NameSub, DoTest, iBlock)
 
-    if (iNewDecomposition /= iLastDecomposition) then
-       iBlockLast         = -100
-       iLastDecomposition = iNewDecomposition
-    end if
+    if(.not.UsePointSource)return
 
     !! Set the source arrays for this block to zero
     SRho_C = 0.
     SP_C = 0.
 
     do iPoint=1, nPointSource
+        ! Check time to see if source is active.
+        ! HERE!
+
+        ! Calculate source:
+        do k=1,nK; do j=1,nJ; do i=1,nI
+           ! For each point, get distance from point.
+           distNow_D = Xyz_DGB(:, i, j, k, iBlock) - XyzSource_DI(:, iPoint)
+           distSource_C(i, j, k) = sum((distNow_D)**2, DIM=1)
+        end do; end do; end do
+
+        ! Apply source as Gaussian about central point.
+        SRho_C(i,j,k) = SRho_C(i,j,k) + (1/(2*cPi*rspread)) * &
+            exp(distSource_C(i,j,k)/(-2*rspread**2))
     end do
 
-    ! Check time to see if source is active.
-    ! HERE!
-
-    ! Calculate source:
+    ! Apply sources to Source_VC
     do k=1,nK; do j=1,nJ; do i=1,nI
-       ! For each point, get distance from point.
-       distSource_C = sqrt(sum((Xyz_GB(:,i, j, k, iBlock) - XyzSource)**2))
-
-       SRho_C =
-    end do; end do; end do
-
-    do k=1,nK; do j=1,nJ; do i=1,nI
-       Source_VC(iRhoIon_I   ,i,j,k) = SRho_IC(i,j,k)    + &
-            Source_VC(iRhoIon_I   ,i,j,k)
-       Source_VC(iPIon_I     ,i,j,k) = SP_IC(1:nIonFluid,i,j,k)      + &
-            Source_VC(iPIon_I     ,i,j,k)
+       Source_VC(Rho_,i,j,k) = SRho_C(i,j,k)    + &
+            Source_VC(Rho_,i,j,k)
+       Source_VC(p_,i,j,k) = SP_C(i,j,k)      + &
+            Source_VC(p_,i,j,k)
     end do;  end do;  end do
 
     call test_stop(NameSub, DoTest, iBlock)
