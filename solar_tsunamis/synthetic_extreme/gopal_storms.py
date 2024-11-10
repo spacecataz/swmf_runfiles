d386b0834c849bf9f9a9f76ff326f3a47cd473d1
Dan Welling <dwelling@umich.edu>
2024-11-10 14:41:35 -0500

    more dev on gopal calculations.

--- solar_tsunamis/synthetic_extreme/gopal_storms.py
+++ solar_tsunamis/synthetic_extreme/gopal_storms.py
@@ -6,6 +6,52 @@ Create estimates for Gopalswamy storms
 
 import numpy as np
 
+import matplotlib.pyplot as plt
+
+
+# Set some constants:
+a, gam, eta = 3.5, 3.35379, 0.5
+
+def weibull(x, a=a, gamma=gam, eta=eta, m=1.0):
+    '''
+    For input `x`, return the corresponding `y` value using a Weibull
+    distribution. Kwargs are Weibull parameters.
+    '''
+
+    X = np.log10(x)
+
+    A1 = (gamma-X)/eta
+
+    Y = a * (1-np.exp(-(A1)**m))
+
+    return 10**Y
+
+
+def inv_weib(y, a=a, gamma=gam, eta=eta, m=1.0):
+    '''
+    Given a value, `y`, return the corresponding `x` as calculated using
+    the inverse of the Weibull function. All parameters are Weibull params.
+    '''
+
+    Y = np.log10(y)
+
+    X = eta * np.log(1 - Y/a) + gamma
+
+    return 10**X
+
+
+def plot_gopal_distros():
+    '''
+    Reproduce the Gopalswamy distribution plots.
+    '''
+
+    dv = 10
+    v = np.arange(100, 4700+dv, dv)
+
+    fig, (a1, a2) = plt.subplots(1, 2)
+
+    a1.loglog(v, weibull(v))
+
 
 def v_plaw(years):
     '''
@@ -26,5 +72,5 @@ def v_weib(years):
 
 def energy_to_dens():
     '''
-    Given an extreme
+    Given an extreme storm
     '''
