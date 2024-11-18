f1d3424183326d69f6b405d7eb08fd35137febd7
Dan Welling <dwelling@umich.edu>
2024-11-18 08:30:21 -0500

    continuning to add capability here

--- solar_tsunamis/synthetic_extreme/gopal_storms.py
+++ solar_tsunamis/synthetic_extreme/gopal_storms.py
@@ -17,6 +17,8 @@ e_a, e_gam, e_eta = 3.1, 32.7, 1.9     # Energy coefficients
 # Characteristics of Katus event:
 vmax_mean = 639.39
 vmax_medi = 612.6
+nmax_mean = 22.05
+nmax_medi = 20.24
 
 
 def v_to_b(v):
@@ -67,8 +69,11 @@ def plot_gopal_distros():
     a1.set_xlabel('Velocity ($km/s$)')
     a1.set_ylabel('Occurrence ($\#/year$)')
 
+    ymin, ymax = a1.get_ylim()
+    a1.vlines(vmax_mean, ymin, ymax, line)
+
     a2.loglog(e, weibull(e, a=e_a, eta=e_eta, gamma=e_gam))
-    a2.set_xlabel('VEnergy ($erg$)')
+    a2.set_xlabel('Energy ($erg$)')
     a2.set_ylabel('Occurrence ($\#/year$)')
 
     fig.tight_layout()
@@ -82,9 +87,15 @@ def v_plaw(years):
     pass
 
 
-def energy_to_dens(v_in):
+def scale_dens(v_in, n_in):
     '''
-    Given an extreme storm velocity, obtain the maximum density.
+    Convert Katus et al max velocity and density into extreme storm densities
+    for the 1/100 and 1/1000 year storms.
+
+    Parameters
+    ----------
+    v_in, n_in : float
+        Velocity and density to scale for extreme event.
     '''
 
     # Energy/velocity for 1/100 and 1/1000 year storm:
@@ -99,6 +110,19 @@ def energy_to_dens(v_in):
           f'occurrence is {occ:.1f} events/year and energy is {ener:.2E} ergs')
 
     # Get scaling factor:
+    gamma100 = e100/ener
+    gamma1k = e1000/ener
+    print(f'Energy scaling factors:\n\tgamma100 = {gamma100}' +
+          f'\n\t gamma1000 = {gamma1k}')
+    print('Velocity ratio factors (v_in/v_ext)**2:')
+    print(f'\t1/100:  {(v_in/v100)**2}')
+    print(f'\t1/1000: {(v_in/v1000)**2}')
+
+    # Scale it up:
+    n100 = n_in * gamma100 * (v_in/v100)**2
+    n1000 = n_in * gamma1k * (v_in/v1000)**2
+
+    return n100, n1000
 
 
 def verify_gopal():
@@ -120,6 +144,18 @@ def verify_gopal():
           f'{inv_weib(1/100)} (3800 expected)')
     print('\tVelocity of 1/1000 storm =  ' +
           f'{inv_weib(1/1000)} (4670 expected)')
+
+    print('Verifying E Weibull:')
+    print('\tOccurrence rate for E=4.4e+33 = ' +
+          f'1/{1/weibull(4.4e+33, a=e_a, eta=e_eta, gamma=e_gam)} (1/100 expected)')
+    print('\tOccurrence rate for E=9.8e+33 = ' +
+          f'1/{1/weibull(9.8e+33, a=e_a, eta=e_eta, gamma=e_gam)} (1/1000 expected)')
+
+    print('Verifying INVERSE E Weibull:')
+    print('\tEnergy of 1/100 storm = ' +
+          f'{inv_weib(1/100, a=e_a, eta=e_eta, gamma=e_gam)} (4.4e+33 expected)')
+    print('\tEnergy of 1/1000 storm =  ' +
+          f'{inv_weib(1/1000, a=e_a, eta=e_eta, gamma=e_gam)} (9.8e+33 expected)')
 
 
 def summarize_extremes():
